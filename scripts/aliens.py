e6cc951a38fc703d9b6aff177983499c6e31e441
selin-engineer <ekinciengineer@gmail.com>
2025-11-26 04:40:34 +0100

    Add alien theories generator function

---
+++ scripts/aliens.py
@@ -0,0 +1,11 @@
+import python
+
+def generate():
+  theories = [
+    "Aliens built the pyramids!",
+    "Aliens live under the ocean!",
+    "Aliens control Wi-Fi signals!",
+    "Aliens disguised themselves as humans in 1995!",
+    "Aliens planted trees to hide their antennas!"
+  ]
+  return random.choice(theories)
