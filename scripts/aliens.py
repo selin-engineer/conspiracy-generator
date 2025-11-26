ea9f8968b1846d6717044c6df8433dedea7408ca
JJovitah12namakula <jovitahnamakula17@gmail.com>
2025-11-26 06:08:17 +0100

    feature2_update.py

--- scripts/aliens.py
+++ scripts/aliens.py
@@ -3,6 +3,9 @@ import python
 def generate():
   theories = [
     "Ronah should be an alien!",
-    "Aliens cry in the night!"
+
+    "Aliens cry in the night!",
+
+    "Aliens can drive car!"
   ]
   return random.choice(theories)
