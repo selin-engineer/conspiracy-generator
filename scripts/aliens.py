d0265b74acef9dc6a774f38d4eb1d41269bfffcd
JJovitah12namakula <jovitahnamakula17@gmail.com>
2025-11-26 04:59:05 +0100

    devel.py

--- scripts/aliens.py
+++ scripts/aliens.py
@@ -2,10 +2,6 @@ import python
 
 def generate():
   theories = [
-    "Aliens built the pyramids!",
-    "Aliens live under the ocean!",
-    "Aliens control Wi-Fi signals!",
-    "Aliens disguised themselves as humans in 1995!",
-    "Aliens planted trees to hide their antennas!"
+    "Aliens live under the ocean!"
   ]
   return random.choice(theories)
